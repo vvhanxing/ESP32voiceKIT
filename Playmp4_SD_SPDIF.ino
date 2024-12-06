0b4e58c73fa8fe9dbec2997a309c6ab527ac8ff2
vvhanxing <33742233+vvhanxing@users.noreply.github.com>
2024-12-06 21:19:40 +0800

    Update Playmp4_SD_SPDIF.ino

--- Playmp4_SD_SPDIF.ino
+++ Playmp4_SD_SPDIF.ino
@@ -1,11 +1,75 @@
+// Example for library:
+// https://github.com/Bodmer/TJpg_Decoder
+
+// This example renders a Jpeg file that is stored in an array within Flash (program) memory
+// see panda.h tab.  The panda image file being ~13Kbytes.
 #include <Arduino.h>
-#include <TFT_eSPI.h>
-#include <TJpg_Decoder.h>
 #include "AudioFileSourceSD.h"
 #include "AudioOutputSPDIF.h"
 // #include "AudioGeneratorFLAC.h"
 #include "AudioGeneratorWAV.h"
 #include "AudioOutputI2S.h"
+
+
+
+
+#define USE_DMA
+// Include the array
+#include "panda.h"
+// Include the jpeg decoder library
+#include <TJpg_Decoder.h>
+#ifdef USE_DMA
+  uint16_t  dmaBuffer1[16*16]; // Toggle buffer for 16*16 MCU block, 512bytes
+  uint16_t  dmaBuffer2[16*16]; // Toggle buffer for 16*16 MCU block, 512bytes
+  uint16_t* dmaBufferPtr = dmaBuffer1;
+  bool dmaBufferSel = 0;
+#endif
+// Include the TFT library https://github.com/Bodmer/TFT_eSPI
+#include "SPI.h"
+#include <TFT_eSPI.h>              // Hardware-specific library
+TFT_eSPI tft = TFT_eSPI();         // Invoke custom library
+
+
+
+
+
+// This next function will be called during decoding of the jpeg file to render each
+// 16x16 or 8x8 image tile (Minimum Coding Unit) to the TFT.
+bool tft_output_dma(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap)
+{
+   // Stop further decoding as image is running off bottom of screen
+  if ( y >= tft.height() ) return 0;
+
+  // STM32F767 processor takes 43ms just to decode (and not draw) jpeg (-Os compile option)
+  // Total time to decode and also draw to TFT:
+  // SPI 54MHz=71ms, with DMA 50ms, 71-43 = 28ms spent drawing, so DMA is complete before next MCU block is ready
+  // Apparent performance benefit of DMA = 71/50 = 42%, 50 - 43 = 7ms lost elsewhere
+  // SPI 27MHz=95ms, with DMA 52ms. 95-43 = 52ms spent drawing, so DMA is *just* complete before next MCU block is ready!
+  // Apparent performance benefit of DMA = 95/52 = 83%, 52 - 43 = 9ms lost elsewhere
+#ifdef USE_DMA
+  // Double buffering is used, the bitmap is copied to the buffer by pushImageDMA() the
+  // bitmap can then be updated by the jpeg decoder while DMA is in progress
+  if (dmaBufferSel) dmaBufferPtr = dmaBuffer2;
+  else dmaBufferPtr = dmaBuffer1;
+  dmaBufferSel = !dmaBufferSel; // Toggle buffer selection
+  //  pushImageDMA() will clip the image block at screen boundaries before initiating DMA
+  tft.pushImageDMA(x, y, w, h, bitmap, dmaBufferPtr); // Initiate DMA - blocking only if last DMA is not complete
+  // tft.dmaWait();  
+  // The DMA transfer of image block to the TFT is now in progress...
+#else
+  // Non-DMA blocking alternative
+  tft.pushImage(x, y, w, h, bitmap);  // Blocking, so only returns when image block is drawn
+#endif
+  // Return 1 to decode next block.
+  return 1;
+}
+
+
+
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
 
 SPIClass CustomSPI;
 #define CS 8
@@ -14,44 +78,45 @@ SPIClass CustomSPI;
 #define MISO_PIN 11
 #define SPI_SPEED SD_SCK_MHZ(40)
 
-TFT_eSPI tft = TFT_eSPI();
+
 AudioFileSourceSD *source = NULL;
 AudioOutputI2S *output = NULL;
-//AudioGeneratorFLAC *decoder = NULL;
+// AudioGeneratorFLAC *decoder = NULL;
 AudioGeneratorWAV *decoder = NULL;
 File picFile;
 File wevFile;
-uint8_t frame_0[1024 * 36] PROGMEM = {0};
+uint8_t frame_0[1024 * 32] PROGMEM = {0};
 size_t fileSize = sizeof(frame_0);
 
 File pic_dir;
 File wav_dir;
 
-bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap) {
-    if (y >= tft.height()) return false;
-    tft.pushImage(x, y, w, h, bitmap);
-    return true;
-}
+
 
 void draw_pic_bin_task(void *pvParameters) {
     while (1) {
         picFile = pic_dir.openNextFile();
         if (picFile) {
             if (String(picFile.name()).endsWith(".bin")) {
-                size_t bytesRead = picFile.readBytes((char *)frame_0, fileSize); // 使用 readBytes 提高读取效率
+                size_t bytesRead = picFile.readBytes((char *)frame_0, fileSize);
                 if (bytesRead > 0) {
+                    tft.startWrite();
                     TJpgDec.drawJpg(0, 0, frame_0, bytesRead);
+                    tft.endWrite();
                 } else {
                     Serial.printf("Error reading binary file: %s\n", picFile.name());
                 }
                 picFile.close();
             }
         } else {
-            pic_dir.rewindDirectory(); // Rewind directory for loop playback
+            pic_dir.rewindDirectory(); // 循环播放
         }
-        vTaskDelay(1 / portTICK_PERIOD_MS); // 减小延迟提升帧率
+        vTaskDelay(2 / portTICK_PERIOD_MS);
     }
 }
+
+
+
 
 void playwav_task(void *pvParameters) {
     while (1) {
@@ -69,61 +134,85 @@ void playwav_task(void *pvParameters) {
                         Serial.printf("Error opening '%s'\n", wevFile.name());
                     }
                 }
-                wevFile.close(); // 确保关闭文件释放资源
+                wevFile.close();
             } else {
-                wav_dir.rewindDirectory(); // Rewind directory for loop playback
+                wav_dir.rewindDirectory();
             }
         }
-        vTaskDelay(10 / portTICK_PERIOD_MS); // 防止任务抢占过多CPU
+        vTaskDelay(2 / portTICK_PERIOD_MS);
     }
 }
 
-void setup() {
-    Serial.begin(115200);
-    delay(1000);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("\n\n Testing TJpg_Decoder library");
+
+  // Initialise the TFT
+  tft.begin();
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.fillScreen(TFT_BLACK);
+
+#ifdef USE_DMA
+  tft.initDMA(); // To use SPI DMA you must call initDMA() to setup the DMA engine
+#else
+  tft.init();
+  
+#endif
+
+  // The jpeg image can be scaled down by a factor of 1, 2, 4, or 8
+  TJpgDec.setJpgScale(1);
+
+  // The colour byte order can be swapped by the decoder
+  // using TJpgDec.setSwapBytes(true); or by the TFT_eSPI library:
+  tft.setSwapBytes(true);
+
+  // The decoder must be given the exact name of the rendering function above
+  TJpgDec.setCallback(tft_output_dma);
 
 
-    tft.init();
-    tft.setRotation(0);
-    tft.fillScreen(TFT_BLACK);
-    TJpgDec.setJpgScale(1);
-    TJpgDec.setSwapBytes(true);// 必须启用以匹配屏幕颜色顺序
-// tft.writecommand(0x26); // 设置伽马
-// tft.writedata(0x04);    // 选择伽马曲线
-    TJpgDec.setCallback(tft_output);
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    tft.setTextSize(2);
-    tft.setCursor(0, 60);
-    tft.println("Say something");
+
+
+
+
+CustomSPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, CS);
+#if defined(ESP8266)
+    SD.begin(SS, SPI_SPEED);
+#else
+    if (!SD.begin(CS, CustomSPI, 40000000)) {
+        Serial.println("SD card initialization failed!");
+    } else {
+        Serial.println("SD card initialized successfully.");
+    }
+#endif
+
+    pic_dir = SD.open("/output/frames");
+    wav_dir = SD.open("/output");
+
+
+
+
+
+
 
     audioLogger = &Serial;
     source = new AudioFileSourceSD();
     output = new AudioOutputI2S();
     decoder = new AudioGeneratorWAV();
 
-    output->SetGain(0.1);
+    output->SetGain(0.5);
     output->SetPinout(40, 42, 17);
 
-    CustomSPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, CS);
-
-    #if defined(ESP8266)
-        SD.begin(SS, SPI_SPEED);
-    #else
-        if (!SD.begin(CS, CustomSPI, 40000000)) {
-            Serial.println("SD card initialization failed!");
-        } else {
-            Serial.println("SD card initialized successfully.");
-        }
-    #endif
-
-    pic_dir = SD.open("/output/frames");
-    wav_dir = SD.open("/output");
 
     // Create tasks for video and audio
-    xTaskCreatePinnedToCore(playwav_task, "PlayWAV", 1024*4, NULL, 1, NULL, 1);
-    xTaskCreatePinnedToCore(draw_pic_bin_task, "DrawPic", 1024*8, NULL, 2, NULL, 0);
+    xTaskCreatePinnedToCore(playwav_task, "PlayWAV", 1024 * 4, NULL,1, NULL, 0);
+    xTaskCreatePinnedToCore(draw_pic_bin_task, "DrawPic", 1024 * 4, NULL, 2, NULL, 1);
+
 }
 
-void loop() {
-    // Loop is intentionally empty as tasks handle playback and video frame rendering
+void loop()
+{
+
+
 }
